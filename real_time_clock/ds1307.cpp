86cc08a2c2f11570b1d724dd5e9fb683b21391a5
Thomas Röggla <tom@lichtfragmente.com>
2023-02-17 16:38:07 +0100

    added wrapper class for DS1307 real time clock

---
+++ real_time_clock/ds1307.cpp
@@ -0,0 +1,97 @@
+#include "ds1307.h"
+
+int DS1307::convertFromBcd(uint8_t bcd) {
+  int tens = ((bcd & 0b11110000) >> 4) * 10;
+  int ones = bcd & 0b00001111;
+
+  return tens + ones;
+}
+
+uint8_t DS1307::convertToBcd(int value) {
+  uint8_t ones = value % 10;
+  uint8_t tens = value / 10;
+
+  return (tens << 4) + ones;
+}
+
+bool DS1307::enableClock(bool enable) {
+  uint8_t* currentValue = (uint8_t*)malloc(sizeof(uint8_t));
+  bool success = device.readByteAtAddress(0x00, currentValue);
+
+  if (!success) {
+    return false;
+  }
+
+  uint8_t enableClockByte = (enable == true) ? (
+    *currentValue & 0b01111111
+  ) : (
+    *currentValue | 0b10000000
+  );
+
+  return device.writeByteAtAddress(0x00, enableClockByte);
+}
+
+bool DS1307::enableOscillator(bool enable, OscillatorFreq freq) {
+  uint8_t enableOsc = 0x0;
+
+  if (enable == true) {
+    enableOsc = 0b00010000;
+
+    switch (freq) {
+      case OscillatorFreq::FREQ_4KHZ:
+        enableOsc = enableOsc | 0b1;
+        break;
+      case OscillatorFreq::FREQ_8KHZ:
+        enableOsc = enableOsc | 0b10;
+        break;
+      case OscillatorFreq::FREQ_32KHZ:
+        enableOsc = enableOsc | 0b11;
+        break;
+      default:
+        break;
+    }
+  }
+
+  return device.writeByteAtAddress(0x07, enableOsc);
+}
+
+bool DS1307::setDateTime(int year, int month, int date, int hours, int minutes, int seconds) {
+  uint8_t* currentSecondValue = (uint8_t*)malloc(sizeof(uint8_t));
+  bool success = device.readByteAtAddress(0x00, currentSecondValue);
+
+  if (!success) {
+    return false;
+  }
+
+  uint8_t newSecondValue = (*currentSecondValue & 0b10000000) | convertToBcd(seconds);
+
+  uint8_t data[7] = {
+    newSecondValue, convertToBcd(minutes), convertToBcd(hours),
+    0,
+    convertToBcd(date), convertToBcd(month), convertToBcd(year % 100)
+  };
+
+  free(currentSecondValue);
+  return device.writeBytesStartingAt(0x00, 7, data);
+}
+
+String DS1307::getFormattedDateTime() {
+  uint8_t result[8];
+  bool success = device.readBytesStartingAt(0x00, 8, result);
+
+  if (success) {
+    int seconds = convertFromBcd(result[0]);
+    int minutes = convertFromBcd(result[1]);
+    int hours = convertFromBcd(result[2]);
+    int date = convertFromBcd(result[4]);
+    int month = convertFromBcd(result[5]);
+    int year = convertFromBcd(result[6]);
+
+    char datetime[20];
+    sprintf(datetime, "20%02d-%02d-%02d %02d:%02d:%02d", year, month, date, hours, minutes, seconds);
+
+    return String(datetime);
+  }
+
+  return "";
+}
---
+++ real_time_clock/ds1307.h
@@ -0,0 +1,32 @@
+#ifndef DS1307_H
+#define DS1307_H
+
+#include <Arduino.h>
+#include "../i2c_device/i2c_device.h"
+
+#define DEVICE_ID 0b1101000
+
+typedef enum class OscillatorFreq {
+  FREQ_1HZ,
+  FREQ_4KHZ,
+  FREQ_8KHZ,
+  FREQ_32KHZ,
+} OscillatorFreq;
+
+class DS1307 {
+  I2CDevice device;
+
+  int convertFromBcd(uint8_t bcd);
+  uint8_t convertToBcd(int value);
+
+public:
+  DS1307(uint8_t sdaPin, uint8_t sclPin) : device(DEVICE_ID, sdaPin, sclPin) {}
+
+  bool enableClock(bool enable);
+  bool enableOscillator(bool enable, OscillatorFreq freq);
+
+  bool setDateTime(int year, int month, int date, int hour, int minute, int second);
+  String getFormattedDateTime();
+};
+
+#endif

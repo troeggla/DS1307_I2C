d1afdbff8f728e168717a420663e3eaa76c01da6
Thomas Röggla <tom@lichtfragmente.com>
2023-02-17 09:08:17 +0100

    added function to set START and STOP conditions

--- main.cpp
+++ main.cpp
@@ -5,6 +5,24 @@
 
 byte deviceId = 0b1101000;
 
+void sendStart() {
+  digitalWrite(SCL, HIGH);
+
+  digitalWrite(SDA, HIGH);
+  digitalWrite(SDA, LOW);
+
+  delay(10);
+  digitalWrite(SCL, LOW);
+}
+
+void sendStop() {
+  digitalWrite(SCL, HIGH);
+
+  digitalWrite(SDA, LOW);
+  delay(10);
+  digitalWrite(SDA, HIGH);
+}
+
 void setup() {
   Serial.begin(115200);
 

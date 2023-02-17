ab3a77093a173849b9102d422f5b67b673eb7c65
Thomas Röggla <tom@lichtfragmente.com>
2023-02-17 10:22:25 +0100

    using class for communicating with the I2C peripheral

--- main.cpp
+++ main.cpp
@@ -1,9 +1,8 @@
 #include <Arduino.h>
-
-#define SDA A4
-#define SCL A5
+#include "i2c_device/i2c_device.h"
 
 byte deviceId = 0b1101000;
+I2CDevice device(deviceId, A4, A5);
 
 int convertFromBcd(int bcd) {
   int tens = ((bcd & 0b11110000) >> 4) * 10;
@@ -19,208 +18,9 @@ byte convertToBcd(int num) {
   return (tens << 4) + ones;
 }
 
-void sendStart() {
-  digitalWrite(SCL, HIGH);
-
-  digitalWrite(SDA, HIGH);
-  digitalWrite(SDA, LOW);
-
-  delay(10);
-  digitalWrite(SCL, LOW);
-}
-
-void sendStop() {
-  digitalWrite(SCL, HIGH);
-
-  digitalWrite(SDA, LOW);
-  delay(10);
-  digitalWrite(SDA, HIGH);
-}
-
-void sendBit(int bit) {
-  if (bit == 0) {
-    digitalWrite(SDA, LOW);
-  } else {
-    digitalWrite(SDA, HIGH);
-  }
-
-  digitalWrite(SCL, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(SCL, LOW);
-  delayMicroseconds(10);
-}
-
-void sendDeviceId(byte address) {
-  for (int i=0; i<7; i++) {
-    int bit = (address << i) & 0x40;
-    sendBit(bit);
-  }
-}
-
-bool readAck() {
-  pinMode(SDA, INPUT);
-  digitalWrite(SCL, HIGH);
-  delayMicroseconds(10);
-
-  int result = digitalRead(SDA);
-  digitalWrite(SCL, LOW);
-  delayMicroseconds(10);
-
-  pinMode(SDA, OUTPUT);
-  return result == 0;
-}
-
-void sendByte(byte data) {
-  for (int i=0; i<8; i++) {
-    int bit = (data << i) & 0x80;
-    sendBit(bit);
-  }
-}
-
-int readByte() {
-  pinMode(SDA, INPUT);
-
-  int result = 0;
-
-  for (int i=0; i<8; i++) {
-    digitalWrite(SCL, HIGH);
-    delayMicroseconds(10);
-
-    int bit = digitalRead(SDA);
-    result = (result << 1) + bit;
-
-    digitalWrite(SCL, LOW);
-    delayMicroseconds(10);
-  }
-
-  pinMode(SDA, OUTPUT);
-  return result;
-}
-
-byte readByteAtAddress(byte deviceId, byte registerAddress) {
-  sendStart();
-  sendDeviceId(deviceId);
-  sendBit(0b0);
-
-  if (!readAck()) {
-    return -1;
-  }
-
-  sendByte(registerAddress);
-
-  if (!readAck()) {
-    return -1;
-  }
-
-  sendStart();
-  sendDeviceId(0b1101000);
-  sendBit(0b1);
-
-  if (!readAck()) {
-    return -1;
-  }
-
-  int byte = readByte();
-
-  sendBit(0b1);
-  sendStop();
-
-  return byte;
-}
-
-bool writeByteAtAddress(byte deviceId, byte registerAddress, byte data) {
-  sendStart();
-  sendDeviceId(deviceId);
-  sendBit(0b0);
-
-  if (!readAck()) {
-    return false;
-  }
-
-  sendByte(registerAddress);
-
-  if (!readAck()) {
-    return false;
-  }
-
-  sendByte(data);
-
-  if (!readAck()) {
-    return false;
-  }
-
-  sendStop();
-  return true;
-}
-
-bool writeBytesStartingAt(byte deviceId, byte registerAddress, int numBytes, byte* data) {
-  sendStart();
-  sendDeviceId(deviceId);
-  sendBit(0b0);
-
-  if (!readAck()) {
-    return false;
-  }
-
-  sendByte(registerAddress);
-
-  if (!readAck()) {
-    return false;
-  }
-
-  for (int i=0; i<numBytes; i++) {
-    sendByte(data[i]);
-
-    if (!readAck()) {
-      return false;
-    }
-  }
-
-  sendStop();
-  return true;
-}
-
-bool readBytesStartingAt(byte deviceId, byte registerAddress, int numBytes, byte* result) {
-  sendStart();
-  sendDeviceId(deviceId);
-  sendBit(0b0);
-
-  if (!readAck()) {
-    return false;
-  }
-
-  sendByte(registerAddress);
-
-  if (!readAck()) {
-    return false;
-  }
-
-  sendStart();
-  sendDeviceId(0b1101000);
-  sendBit(0b1);
-
-  if (!readAck()) {
-    return false;
-  }
-
-  for (int i=0; i<numBytes; i++) {
-    byte data = readByte();
-    result[i] = data;
-
-    if (i != numBytes - 1) {
-      sendBit(0b0);
-    }
-  }
-
-  sendBit(0b1);
-  sendStop();
-
-  return true;
-}
-
 void printTime() {
   byte result[8];
-  bool success = readBytesStartingAt(deviceId, 0x00, 8, result);
+  bool success = device.readBytesStartingAt(0x00, 8, result);
 
   if (success) {
     int seconds = convertFromBcd(result[0]);
@@ -245,33 +45,39 @@ void setup() {
   pinMode(SDA, OUTPUT);
   pinMode(SCL, OUTPUT);
 
+  uint8_t* readByte = (uint8_t*)malloc(sizeof(uint8_t));
+
   Serial.print("Byte at address 0x00: 0b");
-  Serial.println(readByteAtAddress(deviceId, 0x00), BIN);
+  device.readByteAtAddress(0x00, readByte);
+  Serial.println(*readByte, BIN);
 
   Serial.print("Byte at address 0x07: 0b");
-  Serial.println(readByteAtAddress(deviceId, 0x07), BIN);
+  device.readByteAtAddress(0x07, readByte);
+  Serial.println(*readByte, BIN);
+
+  free(readByte);
 
   Serial.print("Writing data at 0x00: ");
-  Serial.println((writeByteAtAddress(deviceId, 0x00, 0b01000000) == true) ? "true" : "false");
+  Serial.println((device.writeByteAtAddress(0x00, 0b00000000) == true) ? "true" : "false");
 
   byte data[7] = { 0, convertToBcd(59), convertToBcd(23), 0, convertToBcd(28), convertToBcd(2), convertToBcd(23) };
 
   Serial.print("Writing block data at 0x00: ");
-  Serial.println((writeBytesStartingAt(deviceId, 0x00, 7, data) == true) ? "true" : "false");
+  Serial.println((device.writeBytesStartingAt(0x00, 7, data) == true) ? "true" : "false");
 
   char txt[] = "Hello World";
   Serial.print("Writing data at 0x10: ");
-  Serial.println((writeBytesStartingAt(deviceId, 0x10, strlen(txt), (byte*)txt) == true) ? "true" : "false");
+  Serial.println((device.writeBytesStartingAt(0x10, strlen(txt), (byte*)txt) == true) ? "true" : "false");
 
   byte buf[12];
-  readBytesStartingAt(deviceId, 0x10, 11, buf);
+  device.readBytesStartingAt(0x10, 11, buf);
   buf[11] = '\0';
 
   Serial.print("Data read from 0x10: ");
   Serial.println(String((char*)buf));
 
   Serial.print("Enabling oscillator: ");
-  Serial.println((writeByteAtAddress(deviceId, 0x07, 0b00010000) == true) ? "true" : "false");
+  Serial.println((device.writeByteAtAddress(0x07, 0b00010000) == true) ? "true" : "false");
 
   Serial.println();
 
